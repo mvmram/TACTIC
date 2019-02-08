157c3d6bf597298897058aa4d8925a72ffdf5995
Vignesh Maharaja Ram M <16348608+mvmram@users.noreply.github.com>
2019-02-08 16:15:15 +0100

    Update watch_drop_folder.py
    this code change uses a different Mail ID(notify_mail_user) to notify in case the Watch Folder is disconnected
    
    the configured user => mail_user is used for mail server authentication while executing the sendmail command - which will fail if the mail_password is not defied.
    
    anyways we wanted different set of users to be notified - so using separate configurations for notifying_mail_user and the other mail_user that has other dependencies...

--- src/tactic/command/watch_drop_folder.py
+++ src/tactic/command/watch_drop_folder.py
@@ -608,8 +608,8 @@ class WatchDropFolderTask(SchedulerTask):
             if self.email_alert:
                 subject = "Watch Folder Error"
                 message = "Timeout Error: Connection to Watch Drop Folder Timed Out."
-                sender_name = Config.get_value("services", "mail_name")
-                sender_email = Config.get_value("services", "mail_user")
+                sender_name = Config.get_value("services", "notify_mail_name")
+                sender_email = Config.get_value("services", "notify_mail_user")
                 recipient_emails = [sender_email]
 
                 email_cmd = SendEmail(sender_email=sender_email, recipient_emails=recipient_emails, msg=message, subject=subject, sender_name=sender_name)
